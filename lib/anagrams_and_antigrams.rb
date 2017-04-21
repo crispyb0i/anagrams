65586e0f287e7a9131bfbae117215e37ca1e18b8
David Shin <xdavidshinx@gmail.com>
2017-04-21 10:45:24 -0700

    logic fix. Now tell if word is anagram or not.

--- lib/anagrams_and_antigrams.rb
+++ lib/anagrams_and_antigrams.rb
@@ -5,6 +5,7 @@ class String
   input2 = word2.downcase.split("")
 
   word_counter = Hash.new()
+  counter_sum = 0
 
   if input1.length != input2.length
     return false
@@ -15,8 +16,25 @@ class String
     word_counter[character] = 1
     elsif word_counter[character] += 1
     end
-
   end
-  return word_counter
+
+  input2.each do |character|
+    if word_counter[character] === nil
+      return false
+    else word_counter[character] -= 1
+    end
+  end
+
+  counter = 0
+  word_counter.each do |value|
+    counter_sum += word_counter.fetch(value[0])
+  end
+
+  if counter_sum === 0
+    return "These words are anagrams"
+  else
+    return false
+  end
+
 end
 end
